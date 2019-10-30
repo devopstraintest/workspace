b201d999e7fdd10a92ddaf4048e57518e264e2b8
Antonio <devopstraintest@gmail.com>
2019-10-30 13:12:24 +0100

    proba

--- src/collection/mainFile.groovy
+++ src/collection/mainFile.groovy
@@ -4,7 +4,12 @@ package collection
 
 public class MainClass {
     public static void main( String[] args ) {
-        def reporting = TestClass.PackageName
-        print(reporting.values().collect { it.name }.sort())
+//        def reporting = TestClass.PackageName
+//        print(reporting.values().collect { it.name }.sort())
+        def baseUrl = "https://24.sata.hr"
+        def url = new URL(baseUrl)
+        println("Protocol: "+url.getProtocol());
+        println("Host Name: "+url.getHost());
+        println("Port Number: "+url.getPort());
     }
 }
